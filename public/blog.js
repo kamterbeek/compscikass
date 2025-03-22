9eaa5ebdf122d48dd0273c1affaf829a37a2f981
Kassandra <kam.terbeek@gmail.com>
2025-03-22 16:18:09 -0600

    Create blog.js

---
+++ public/blog.js
@@ -0,0 +1,12 @@
+// Get the h2 element
+const h2Element = document.getElementById('bubble-heading');
+
+// Add event listener to trigger the effect
+h2Element.addEventListener('mouseover', () => {
+    h2Element.classList.add('grow'); // Add 'grow' class on hover
+});
+
+// Remove the effect when mouse leaves
+h2Element.addEventListener('mouseout', () => {
+    h2Element.classList.remove('grow'); // Remove 'grow' class
+});
